8e5417783a25613e5802856886fff4b0b9cce5ad
David Shelton <david.faria012@gmail.com>
2024-01-24 20:50:16 -0300

    Add dropTable

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ def delete(userid):
 
     return Response(response=json.dumps({"status": "success", "data": usuario.to_dict()}), status=200, content_type="application/json")
 
-@app.route("/dropTable", methods=["DELETE"])
+@app.route("/dropTable", methods=["DELETE", "GET"])
 def dropTable():
     db.session.execute("DROP TABLE usuario")
     db.session.commit()
